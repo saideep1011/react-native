f18406513053a13836eb445f571fa4c878a6da22
saideep1011 <saideep2311@gmail.com>
2020-08-24 17:26:47 +0530

    componentsscreen

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -2,14 +2,16 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
 import ListScreen from "./src/screens/ListScreen"
+import ComponentsScreen from './src/screens/ComponentsScreen';
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Components:ComponentsScreen,
     List: ListScreen
   },
   {
-    initialRouteName: "List",
+    initialRouteName: "Components",
     defaultNavigationOptions: {
       title: "App"
     }
