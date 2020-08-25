daa25791cb57afeb4006dbcb9bd420c256ccf01c
saideep1011 <saideep2311@gmail.com>
2020-08-25 15:16:49 +0530

    imgages

--- rn-starter/src/components/ImageDetail.js
+++ rn-starter/src/components/ImageDetail.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text ,Image} from 'react-native';
 
 export default function ImageDetail(props) {
     return (
         <View>
+            <Image source={require('../../assets/beach.jpg')}/>
             <Text>{props.title}</Text>
         </View>
     )
