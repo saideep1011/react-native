442da6e675565a435fe22a2eedf7909bd324a6cf
saideep1011 <saideep2311@gmail.com>
2020-08-25 12:22:59 +0530

    Destructuring props

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Text, StyleSheet ,View,Button,TouchableOpacity} from 'react-native';
 
-const HomeScreen = (props) => {
+const HomeScreen = ({navigation}) => {
   return <View>
     <Text style={styles.text}>Hi ssss!</Text>
-    <Button 
+    <Button onPress={()=>navigation.navigate('Components') }
     title=
     "Go to Components Screen"/>
-    <TouchableOpacity onPress={()=>props.navigation.navigate('Components') }>
+    <TouchableOpacity onPress={()=>navigation.navigate('List') }>
       <Text>go to list demo</Text>
     </TouchableOpacity>
 
