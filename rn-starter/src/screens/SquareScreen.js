aad2064454e9cb7f159badf3be1b552edfc9cdc2
saideep1011 <saideep2311@gmail.com>
2020-08-28 19:17:24 +0530

    changing state  of colours

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -8,15 +8,22 @@ export default function SquareScreen() {
     const [Red, setRed]=useState(0);
     const [Blue, setBlue]=useState(0);
     const [Green, setGreen]=useState(0);
-    console.log(Red);
+    console.log(Blue);
+    
     return (
         <View>
             
             <ColourCounter colour="Red" 
-            onIncrease ={() => setRed(Red+1)}
-            onDecrease = {() => setRed(Red-1)}/>
-            <ColourCounter colour="Blue"/>
-            <ColourCounter colour="Green"/>
+            onIncrease ={() => setRed(Red+10)}
+            onDecrease = {() => setRed(Red-10)}/>
+            <ColourCounter colour="Blue"
+            onIncrease ={() => setBlue(Blue+10)}
+            onDecrease = {() => setBlue(Blue-10)}
+            />
+            <ColourCounter colour="Green"
+            onIncrease ={() => setGreen(Green+10)}
+            onDecrease = {() => setGreen(Green-10)}/>
+            <View style={{ height:100, width:100, backgroundColor:`rgb(${Red}, ${Green}, ${Blue})`}}/>
         </View>
     )
 }
